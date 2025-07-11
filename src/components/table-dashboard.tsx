78f3afa51749b9637a4a9ae9b4b95439b1a4262f
segnitsega <segnitsega6@gmail.com>
2025-07-11 09:40:32 +0300

    adjust table-dashboard styling

--- src/components/table-dashboard.tsx
+++ src/components/table-dashboard.tsx
@@ -3,7 +3,7 @@ import book from "../assets/book.avif";
 const TableDashboard = () => {
   return (
     <div>
-      <table className="m-8">
+      <table className="table-auto m-8 border-separate border-spacing-x-4 border-spacing-y-2">
         <tr className="border-b border-gray-300 shadow">
           <th>Cover Picture</th>
           <th>Title</th>
@@ -12,34 +12,34 @@ const TableDashboard = () => {
           <th>Stock</th>
           <th>Description</th>
         </tr>
-        <tr className="">
+        <tr>
           <td>
-            <img src={book} alt="book-cover" className="w-[50px] h-[50px] rounded-lg my-2" />
+            <img src={book} alt="book-cover" className="w-[50px] h-[50px] rounded-lg" />
           </td>
           <td className="">Dammy Book</td>
-          <td className="">MR.Author</td>
+          <td className="">MR.Author </td>
           <td>200USD</td>
-          <td>10</td>
+          <td>10 Copies</td>
           <td>Enjoyable dummy book</td>
         </tr>
         <tr>
           <td>
-            <img src={book} alt="book-cover" className="w-[50px] h-[50px] rounded-lg my-2" />
+            <img src={book} alt="book-cover" className="w-[50px] h-[50px] rounded-lg" />
           </td>
           <td>Dammy Book</td>
           <td>MR.Author</td>
           <td>200USD</td>
-          <td>10</td>
+          <td>10 Copies</td>
           <td>Enjoyable dummy book</td>
         </tr>
         <tr>
           <td>
-            <img src={book} alt="book-cover" className="w-[50px] h-[50px] rounded-lg my-2" />
+            <img src={book} alt="book-cover" className="w-[50px] h-[50px] rounded-lg" />
           </td>
           <td>Dammy Book</td>
           <td>MR.Author</td>
           <td>200USD</td>
-          <td>10</td>
+          <td>10 Copies</td>
           <td>Enjoyable dummy book</td>
         </tr>
       </table>
